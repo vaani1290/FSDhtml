a65d9b76aafeb2ab8eb180d5b2b17d7cc8fc016a
vaani1290 <vaanibatan@gmail.com>
2024-11-14 19:18:02 +0530

    js work

--- CSSWORK/Js work/index.js
+++ CSSWORK/Js work/index.js
@@ -1,37 +1,79 @@
-//console.log("hello world");
-//const a=12;
-//if(a>10){
-   // let b=20;
-   // a=40;
-   // console.log("a inside:"+a); 
-//}
-//console.log("a outside:"+b);//
-//console.log("Hi, inside index.js");
+// console.log("hello world");
+
+// var a =12;
+// if(a>10){
+//     b=20;
+//     console.log("a inside:"+a);
+// }
+// console.log("a outside:"+b);
+//console.log("hi, inside index.js");
 //function fun(a,b){
- //console.log("Hii");
- //return a+b;
+  //  console.log("hiiii");
+    //return a+b;
 //}
-//const result=fun(23,12);
+//const result = fun(23,12);
 //console.log(result);
-const btn=document.getElementById("btn");
-console.dir(btn);
- function selectLanguage(language)
-{
-   console.log("hi,inside selectLanguage");
-   let data;
-   if(language=='c'){
-   function cCompiler()
-   {
-    return "C Compiler ";
-   }
-   data==cCompiler();
-}
-if(language=="java"){
-    function javaCompiler(){
-     return "Java Compiler";
-    }
-    data==javaCompiler();
-   }
- return data;
-}
+//const button = document.getElementById("btn");
+//const output=document.getElementById('output');
+//const select=document.getElementById('select');
+
+//console.log(select,value);
+//console.dir(output);
+
+//function selectlanguage(language){
+   // console.log("Hi, Inside Language")
+    //output.innerHTML="<h2>Welcome to home page</h2>"
+    //let data;
+    //if(language=='c'){
+//function cCompiler()
+//{
+  //  return "C compiler"
+//}
+//data=cCompiler()
+   // }
+    //if(language=='java'){  
+//function javacompiler()
+//{
+   // return "java compiler"
+//}
+//data = javacompiler()
+    //}
+    //return data;
+//}
+//button.addEventListener("click",()=>{
+//   const comp=selectlanguage('java');
+  // output.innerHTML=`<h2 style=color:cyan>${comp}</h2>`
+   //console.log(comp);
+//});
 //console.log(selectLanguage("c"));
+//const mypromise=new Promise(
+   //(resolve,reject)=>{
+    //  let a=12;
+    //  if(a>10){
+        // resolve({name:"rahul",branch:'IT'});
+
+      //}
+      //else{
+       //  reject("data is rejected")
+      //}
+   //}
+//); 
+//mypromise.then(msg=>console.log(msg.branch))
+//.catch(error=>console.log(error))
+//.finally(()=>console.log("finally Executed"));
+function getdata(){
+const output=document.getElementById('output');
+const data=fetch("https://dummyjson.com/recipes");
+data.then(
+   (res)=>{
+      console.log(res);
+      res.json().then(
+         (response)=>{
+            console.log(response.recipes[0].name);
+            console str=response.recipes[0].id+ " "+response.recipes[0].name+ "  "response.recipes[0].instruction;
+
+            output.innerHTML=`${str}`;
+         }
+      )
+   }
+)}
